741f4123715acb9af6eeffc18b611c8c8a3e9754
Mai Hoa <hoa.le22@student.passerellesnumeriques.org>
2021-11-20 02:08:01 +0700

    06_Handling event

--- main.js
+++ main.js
@@ -4,15 +4,22 @@ var vueInstance = new Vue ({
         title: 'Quần',
         url: 'https://www.youtube.com/watch?v=AHPkqa5ZaN0&list=PLv6GftO355AtDjStqeyXvhA1oRLuhvJWf&index=7&ab_channel=ZendVN-H%E1%BB%8DcL%E1%BA%ADpTr%C3%ACnhOnline',
         target:'_blank',
-        price: 10000
+        price: 10000,
+        counter:0
     },
     methods:{
         say:function(text){
             return "Hello " +text;
         },
+
         formatPrice(){
             var number = this.price;
             return new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'VND' }).format(number);
+        },
+
+        handerClick(e, number){
+            console.log('click', e);
+            this.counter += number;
         }
     }
 });

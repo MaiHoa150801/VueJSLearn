a8f31ae6c718512c58407a82252ed1e18c72e9ab
Mai Hoa <hoa.le22@student.passerellesnumeriques.org>
2021-11-20 01:49:03 +0700

    05_data binding

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 var vueInstance = new Vue ({
     el:'#app',
     data:{
-        title: 'Hello',
+        title: 'Quần',
+        url: 'https://www.youtube.com/watch?v=AHPkqa5ZaN0&list=PLv6GftO355AtDjStqeyXvhA1oRLuhvJWf&index=7&ab_channel=ZendVN-H%E1%BB%8DcL%E1%BA%ADpTr%C3%ACnhOnline',
+        target:'_blank',
+        price: 10000
     },
     methods:{
         say:function(text){
             return "Hello " +text;
+        },
+        formatPrice(){
+            var number = this.price;
+            return new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'VND' }).format(number);
         }
     }
 });
